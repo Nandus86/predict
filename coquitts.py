9c68a1c3e13b528ea9b3bbd02cd4e1050bd5936d
Nandus86 <164101455+Nandus86@users.noreply.github.com>
2024-09-11 23:16:36 -0300

    Update coquitts.py

--- coquitts.py
+++ coquitts.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from gradio_client import Client
 
@@ -20,19 +20,23 @@ def predict(data: InputData):
     # Cria o cliente Gradio
     client = Client("https://coquitts.nandus.com.br/")
     
-    # Executa a predição usando os dados da requisição
-    result = client.predict(
-        data.text_prompt,  # str  in 'Texto do Prompt'
-        data.language,  # str in 'Idioma'
-        data.audio_reference,  # str in 'Áudio de Referência'
-        data.use_microphone,  # str in 'Use microfone para referência'
-        data.clean_voice,  # bool in 'Limpar Voz de Referência'
-        data.no_auto_detect,  # bool in 'Não usar detecção automática de idioma'
-        data.agree,  # bool in 'Concordo'
-        data.fn_index
-    )
-    
-    return {"result": result}
+    try:
+        # Executa a predição usando os dados da requisição
+        result = client.predict(
+            data.text_prompt,  # str  in 'Texto do Prompt'
+            data.language,  # str in 'Idioma'
+            data.audio_reference,  # str in 'Áudio de Referência'
+            data.use_microphone,  # str in 'Use microfone para referência'
+            data.clean_voice,  # bool in 'Limpar Voz de Referência'
+            data.no_auto_detect,  # bool in 'Não usar detecção automática de idioma'
+            data.agree,  # bool in 'Concordo'
+            fn_index=data.fn_index  # Garanta que fn_index esteja correto
+        )
+        return {"result": result}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"ValueError: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 if __name__ == "__main__":
     import uvicorn
