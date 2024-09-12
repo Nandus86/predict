8b1bd59228dbc47f087f9a1096f75040aa2b13b9
Nandus86 <164101455+Nandus86@users.noreply.github.com>
2024-09-11 23:34:41 -0300

    Update coquitts.py

--- coquitts.py
+++ coquitts.py
@@ -30,7 +30,7 @@ def predict(data: InputData):
             data.clean_voice,  # bool in 'Limpar Voz de Referência'
             data.no_auto_detect,  # bool in 'Não usar detecção automática de idioma'
             data.agree,  # bool in 'Concordo'
-            data.fn_index  # Garanta que fn_index esteja correto
+            fn_index=data.fn_index  # Garanta que fn_index esteja correto
         )
         return {"result": result}
     except ValueError as e:
