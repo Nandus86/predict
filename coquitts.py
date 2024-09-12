b24964a3b82bf438b57f3e08e3dcd88ea5664169
Nandus86 <164101455+Nandus86@users.noreply.github.com>
2024-09-11 23:24:45 -0300

    Update coquitts.py

--- coquitts.py
+++ coquitts.py
@@ -11,7 +11,6 @@ class InputData(BaseModel):
     clean_voice: bool
     no_auto_detect: bool
     agree: bool
-    fn_index: int
 
 app = FastAPI()
 
@@ -30,7 +29,7 @@ def predict(data: InputData):
             data.clean_voice,  # bool in 'Limpar Voz de Referência'
             data.no_auto_detect,  # bool in 'Não usar detecção automática de idioma'
             data.agree,  # bool in 'Concordo'
-            data.fn_index  # Garanta que fn_index esteja correto
+            data.fn_index=1  # Garanta que fn_index esteja correto
         )
         return {"result": result}
     except ValueError as e:
