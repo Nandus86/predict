f0bb914040f7c01b18b4b1eafd2856e2cbb28da0
Nandus86 <164101455+Nandus86@users.noreply.github.com>
2024-09-17 17:04:56 -0300

    Update coquitts.py

--- coquitts.py
+++ coquitts.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI, HTTPException
+import os
+import shutil
+from fastapi import FastAPI
 from pydantic import BaseModel
 from gradio_client import Client
 
-class DadosEntrada(BaseModel):
+app = FastAPI()
+
+client = Client("https://coquitts.nandus.com.br/")
+
+class PredictRequest(BaseModel):
     text_prompt: str
     language: str
     audio_reference: str
@@ -13,30 +19,37 @@ class DadosEntrada(BaseModel):
     fn_index: int
     usar_mic: bool
 
-app = FastAPI()
-
-# Atualizar a URL do cliente Gradio
-cliente = Client("https://coquitts.nandus.com.br/")
-
 @app.post("/predict")
-async def prever(dados: DadosEntrada):
+async def predict(request_data: PredictRequest):
     try:
-        resultado = cliente.predict(
-            dados.text_prompt,
-            dados.language,
-            dados.audio_reference,
-            dados.use_microphone,
-            dados.usar_mic,
-            dados.clean_voice,
-            dados.no_auto_detect,
-            dados.agree,
-            fn_index=dados.fn_index
+        # Chama o Gradio Client para fazer a predição
+        result = client.predict(
+            request_data.text_prompt,
+            request_data.language,
+            request_data.audio_reference,
+            request_data.use_microphone,
+            request_data.usar_mic,
+            request_data.clean_voice,
+            request_data.no_auto_detect,
+            request_data.agree,
+            fn_index=request_data.fn_index
         )
-        return {"resultado": resultado}
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+
+        # Filtra o arquivo .wav do resultado
+        wav_file_path = [r for r in result if r.endswith('.wav')][0]
+
+        # Define o diretório de destino para o arquivo final
+        final_directory = "/app/final_audio/"
+        os.makedirs(final_directory, exist_ok=True)
+
+        # Move o arquivo .wav para o diretório final e apaga temporários
+        final_wav_path = shutil.move(wav_file_path, final_directory)
+        temp_dir = os.path.dirname(wav_file_path)
+        shutil.rmtree(temp_dir)  # Apaga o diretório temporário
+
+        return {"result": final_wav_path}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
